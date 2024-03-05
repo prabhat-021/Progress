cab4a1b76397c1df4bdfb6f457c3e0e3075b0647
Ashwin Yadav <104228697+ashwin7455@users.noreply.github.com>
2024-03-06 01:08:14 +0530

    navbar structure

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,8 @@
+import StickyNavbar from "../../components/Navbar/Navbar";
+// import Navbar from "../../components/Navbar/Navbar";
+
+export default function Home() {
+    return (
+        <StickyNavbar />
+    );
+}
--- client/src/pages/Home/Home.jsx
+++
@@ -1,7 +0,0 @@
-import Navbar from "../../components/Navbar/Navbar";
-
-export default function Home() {
-    return (
-        <Navbar />
-    );
-}
