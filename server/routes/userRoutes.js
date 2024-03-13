75f9e6c5ffa96fb27469028bb3e1417b1f8cd138
prabhat-021 <prabhatsahrawat010203@gmail.com>
2024-03-13 20:57:02 +0530

    Validator

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { registerUser, authUser} = require("../controllers/userControllers.js");
+const { registerUser, authUser } = require("../controllers/userControllers.js");
 const auth = require("../middleware/auth.js");
+const { validateUser, validate } = require("../middleware/validator.js");
 
 const router = express.Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(validateUser, validate, registerUser);
 router.route("/login").post(authUser);
 // router.route("/profile").post(auth, updateUserProfile);
 
