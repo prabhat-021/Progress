b488b9646910753395d83ef4eb433d0589fe4311
Ashwin Yadav <104228697+ashwin7455@users.noreply.github.com>
2024-03-15 20:26:42 +0530

    added new pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import Home from "./pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/Home/About";
+import Contact from "./pages/Home/Contact"
 
 function App() {
   return (
@@ -7,9 +9,9 @@ function App() {
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<Home />} exact/>
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/contact" element={<Contact />} /> */}
+        <Route path="/about" element={<About/>}   exact/>
+        {/* <Route path="/project" element={<Project />} /> */}
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
