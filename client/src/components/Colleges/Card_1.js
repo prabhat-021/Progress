b2b269a44132dffa6192985647a6149291035d7f
Ashwin Yadav <104228697+ashwin7455@users.noreply.github.com>
2024-04-18 17:22:38 +0530

    changes in BACKGROUND

--- client/src/components/Colleges/Card_1.js
+++ client/src/components/Colleges/Card_1.js
@@ -1,4 +1,5 @@
 import React from "react";
+import img2 from '../../assets/cardimg.avif'
 
 import {
   Card,
@@ -9,22 +10,22 @@ import {
   Button,
   Tooltip,
 } from "@material-tailwind/react";
-// import img2 from '../../assets/cardimg.avif'
+
 const Card_1 = ({ name }) => {
   return (
     <div>
       <Card className="w-full  max-w-[22rem] shadow-lg">
         <CardHeader floated={false} color="blue-gray">
           <img
-            src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            src={img2}
             alt="ui/ux review check"
           />
           <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
         </CardHeader>
         <CardBody>
-          <div className="flex items-center justify-between h-[80%">
+          <div className="flex items-center justify-between h-[80%]">
             <Typography variant="h5" color="blue-gray" className="font-medium">
-              Wooden House, Florida
+             {name}
             </Typography>
             <Typography
               color="blue-gray"
@@ -116,16 +117,16 @@ const Card_1 = ({ name }) => {
                 </svg>
               </span>
             </Tooltip>
-            <Tooltip content="And +20 more">
+            {/* <Tooltip content="And +20 more">
               <span className="cursor-pointer rounded-full border border-gray-900/5 bg-gray-900/5 p-3 text-gray-900 transition-colors hover:border-gray-900/10 hover:bg-gray-900/10 hover:!opacity-100 group-hover:opacity-70">
                 +20
               </span>
-            </Tooltip>
+            </Tooltip> */}
           </div>
         </CardBody>
         <CardFooter className="pt-3">
           <Button className="bg-black h-9" size="lg" fullWidth={true}>
-            Reserve
+            EXPLORE
           </Button>
         </CardFooter>
       </Card>
