40cbd2a27d8115671776e676073b54484b05d302
Ashwin <ashwinyaadav@gmail.com>
2025-02-21 02:24:07 +0530

    map added

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -9,7 +9,17 @@ const Contact = () => {
       </div>
 
       <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm">
-        <img className="w-full md:max-w-[360px]" src={assets.contactus} alt="" />
+        {/* <img className="w-full md:max-w-[360px]" src={assets.contactus} alt="" /> */}
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3497.873742274402!2d77.49449147496122!3d28.753186378592385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cf574d18f2b6f%3A0x4a65c0bc0122eb2f!2sKIET%20Group%20of%20Institutions!5e0!3m2!1sen!2sin!4v1740084108474!5m2!1sen!2sin"
+          width="560"
+          height="350"
+          style={{ border: 0}}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+
         <div className="flex flex-col justify-center items-start gap-6">
           <p className=" font-semibold text-lg text-gray-600">OUR UNIVERSITY</p>
           <p className=" text-gray-500">E-BLOCK <br /> Kiet Group Of Institutions , Muradnagar ,Ghaziabad</p>
