482daa9ea464d93cd5af1e10e9713381d8cc8df7
Prabhat Sehrawat <prabhatsahrawat010203@gmail.com>
2025-06-04 12:15:01 +0530

    deplyment

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,27 @@ connectCloudinary();
 // middlewares
 app.use(express.json());
 
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true
+// }));
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://progress-b8v3.vercel.app'
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
+
 
 app.use(cookieParser());
 
