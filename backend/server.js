282dcec1dd9bf8b50233a0686eea77fe897048c8
Prabhat Sehrawat <prabhatsahrawat010203@gmail.com>
2025-06-04 14:48:36 +0530

    deplyment-final

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,12 @@ connectCloudinary();
   ];
   
 const corsOptions = {
-  origin: (origin, callback) => {
-    callback(null, true); 
+   origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
   },
   credentials: true,
 };
